57760f7b28adb6b59729423f43c2684430ae0daa
Maximo <91247@sistemas.frc.utn.edu.ar>
2023-05-14 12:41:07 -0300

    Cammbio en rama nueva

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const personajesDBZ = require("./rutas/personajes");
 app.use(personajesDBZ)
 
-const port = 3000;
+const port = 2000; // Modificado
 
 app.listen(port, () => {
     console.log("SERVIDOR INICIADO!")
